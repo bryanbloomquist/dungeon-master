cd65a4e9dca6ff25ae04a4fbe7816c1f16597426
Bryan Bloomquist <bmbloomquist@gmail.com>
2021-03-17 23:25:10 -0500

    table styling

--- src/Components/MainBody/CombatTable/CombatTable.js
+++ src/Components/MainBody/CombatTable/CombatTable.js
@@ -11,38 +11,40 @@ const CombatTable = () => {
 		handleDeathButton,
 	} = useContext(Context);
 	return (
-		<table className='combatTable' id='combatTable'>
-			<thead className='combatTable__header'>
-				<tr>
-					<th>Init.</th>
-					<th>Name</th>
-					<th>A.C.</th>
-					<th>H.P.</th>
-					<th>Amount (+/-)</th>
-					<th>Heal</th>
-					<th>Damage</th>
-					<th>Dead</th>
-				</tr>
-			</thead>
-			<tbody id='combatTable-Body'>
-				{tableData
-					? tableData.map((char) => (
-							<TableRow
-								key={char.key}
-								init={char.init}
-								name={char.name}
-								armr={char.armr}
-								dmge={char.dmge}
-								hlth={char.hlth}
-								changed={handleValueChange}
-								healed={() => handleHealthButton(char.key)}
-								attacked={() => handleDmgeButton(char.key)}
-								killed={() => handleDeathButton(char.key)}
-							/>
-					  ))
-					: null}
-			</tbody>
-		</table>
+		<div class='combatTable__container'>
+			<table className='combatTable' id='combatTable'>
+				<thead className='combatTable__header'>
+					<tr>
+						<th>Init.</th>
+						<th>Name</th>
+						<th>A.C.</th>
+						<th>H.P.</th>
+						<th>(+/-)</th>
+						<th>Heal</th>
+						<th>Damage</th>
+						<th>Dead</th>
+					</tr>
+				</thead>
+				<tbody id='combatTable-Body'>
+					{tableData
+						? tableData.map((char) => (
+								<TableRow
+									key={char.key}
+									init={char.init}
+									name={char.name}
+									armr={char.armr}
+									dmge={char.dmge}
+									hlth={char.hlth}
+									changed={handleValueChange}
+									healed={() => handleHealthButton(char.key)}
+									attacked={() => handleDmgeButton(char.key)}
+									killed={() => handleDeathButton(char.key)}
+								/>
+						  ))
+						: null}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
