85798a52cc2db5834944c0273cefd4273cf0062b
Bryan Bloomquist <bmbloomquist@gmail.com>
2021-03-26 00:01:04 -0500

    build deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "./AppContext";
 import Banner from "./Components/Banner/Banner";
 import Login from "./Components/Login/Login";
-import Confirm from "./Components/Login/Confirm/Confirm";
+import Confirm from "./Components/Confirm/Confirm";
 import MainBody from "./Components/MainBody/MainBody";
 import Modal from "./Components/Modal/Modal";
 
