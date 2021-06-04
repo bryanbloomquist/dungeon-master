d466cfdb67d4a7d3f7b886adc0a13ffed1b3ad4b
Bryan Bloomquist <bmbloomquist@gmail.com>
2021-06-04 11:55:47 -0500

    update to dice roller

--- src/Components/MainBody/Sidebar/Accordion/DiceRoller/DiceRoller.js
+++ src/Components/MainBody/Sidebar/Accordion/DiceRoller/DiceRoller.js
@@ -10,6 +10,7 @@ class DiceRoller extends Component {
 			numSides: 4,
 			modifier: 0,
 			numDiceArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+			numSidesArray: [4, 6, 8, 10, 12, 20, 100],
 			line1: "[ Dice ]",
 			line2: "[ Results ]",
 			line3: "[ Total ]",
@@ -37,13 +38,18 @@ class DiceRoller extends Component {
 			.then((response) => {
 				const modifier = this.state.modifier;
 				let details = response.data.details.split("");
-				details.splice(0, 2);
-				details.splice(-2, 2);
+				if (this.state.numDice > 1) {
+					details.splice(0, 2);
+					details.splice(-2, 2);
+				}
 				const input = response.data.input;
 				const result = response.data.result;
 				const total = parseInt(result) + modifier;
-				const line1 = input + "+" + modifier;
-				const line2 = details.join("").replace(/ /g, "") + "+" + modifier;
+				const line1 = modifier > 0 ? input + "+" + modifier : input;
+				const line2 =
+					modifier > 0
+						? "[" + details.join("").replace(/ /g, "") + "] +" + modifier
+						: details.join("").replace(/ /g, "");
 				const line3 = "Total: " + total;
 				this.setState({ line1: line1, line2: line2, line3: line3 });
 			});
@@ -55,32 +61,27 @@ class DiceRoller extends Component {
 				<h2>Dice Roller</h2>
 				<label htmlFor='numDice'>Number of Dice:</label>
 				<select id='numDice' onChange={(el) => this.changeNumDice(el)}>
-					{this.state.numDiceArray.slice(1).map((num) => {
-						return (
-							<option key={num} value={num}>
-								{num}
-							</option>
-						);
-					})}
+					{this.state.numDiceArray.slice(1).map((num) => (
+						<option key={num} value={num}>
+							{num}
+						</option>
+					))}
 				</select>
 				<label htmlFor='numSides'>Number of Sides:</label>
 				<select id='numSides' onChange={(el) => this.changeNumSides(el)}>
-					<option value='4'>4</option>
-					<option value='6'>6</option>
-					<option value='8'>8</option>
-					<option value='10'>10</option>
-					<option value='12'>12</option>
-					<option value='20'>20</option>
+					{this.state.numSidesArray.map((num) => (
+						<option key={num} value={num}>
+							{num}
+						</option>
+					))}
 				</select>
 				<label htmlFor='modifer'>Modifier</label>
 				<select id='modifier' onChange={(el) => this.changeModifier(el)}>
-					{this.state.numDiceArray.map((num) => {
-						return (
-							<option key={num} value={num}>
-								{num}
-							</option>
-						);
-					})}
+					{this.state.numDiceArray.map((num) => (
+						<option key={num} value={num}>
+							{num}
+						</option>
+					))}
 				</select>
 				<Button clicked={this.rollDice} title='Roll Dice' />
 				<div className='diceTray'>
